4335a4b6e07a65ec5523a37f0fa41a578a903556
Zhanarys17 <zhanarys.d7@gmail.com>
2022-03-03 12:40:08 +0600

    1 Lecture task of Diego

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Zhanarys");
+        String name1 = "Zhan";
+        String name2 = "Asd";
+        String name3 = "Qwe";
+
+        /*for (int i=0;i<10;i++){
+            System.out.println("Когда я вырасту, то хочу быть паровым экскаватором!");
+        }*/
+        System.out.println("Happy New Year");
     }
 
 }
